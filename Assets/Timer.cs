0e81e40ecfc8e846076f0414f52c93a52ad30d45
Rafael1991 <n-fabrizius@web.de>
2018-11-22 10:54:25 +0100

    removed examples pictures and countdown
    need pictures and printed cards to proceed

---
+++ Assets/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text gameTimerText;
+    float gameTimer = 0f;
+    
+	
+	// Update is called once per frame
+	void Update () {
+        gameTimer += Time.deltaTime;
+
+        int seconds = (int)(gameTimer % 60);
+        int minutes = (int)(gameTimer / 60) % 60;
+
+        string timerString = string.Format("{1:00}:{2:00}", minutes, seconds);
+
+        gameTimerText.text = timerString;
+	}
+}
